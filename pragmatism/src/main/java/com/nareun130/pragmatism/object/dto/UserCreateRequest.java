e4ef8e5a38e97132c288ce70e86bc9767f9ba379
김성호 <nareun130@gmail.com>
2024-07-16 23:35:07 +0900

    feat : DTO 예제 -> DTO는 단순히 데이터를 전달한다.

--- pragmatism/src/main/java/com/nareun130/pragmatism/object/dto/UserCreateRequest.java
+++ pragmatism/src/main/java/com/nareun130/pragmatism/object/dto/UserCreateRequest.java
@@ -1,16 +1,45 @@
 package com.nareun130.pragmatism.object.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
 // * 이런 방법은 결국 모든 멤버변수를 public으로 선언한 거나 다름 없음, 오히려 접근이 더 불편
 @Getter
 @Setter
 public class UserCreateRequest {
 
-    private String username;
-    private String password;
-    private String email;
-    private String address;
-    private String gender;
-    private int age;
+    // private String username;
+    // private String password;
+    // private String email;
+    // private String address;
+    // private String gender;
+    // private int age;
+
+    public final String username;
+    public final String password;
+    public final String email;
+    public final String address;
+    public final String gender;
+    public final int age;
+
+    //* DTO의 데이터를 JSON같은 형식으로 직렬화/역질렬화 할 수 있으며, 최초에 데이터가 할당된 이후 불변하게 해서 데이터 보호한다.
+    @Builder
+    public UserCreateRequest(
+            @JsonProperty("username") String username,
+            @JsonProperty("password") String password,
+            @JsonProperty("email") String email,
+            @JsonProperty("address") String address,
+            @JsonProperty("gender") String gender,
+            @JsonProperty("age") int age) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.address = address;
+        this.gender = gender;
+        this.age = age;
+    }
+
 }
