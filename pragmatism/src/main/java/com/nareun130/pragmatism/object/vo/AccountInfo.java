6de397979ed9708610c077ff44753b486b93b14e
김성호 <nareun130@gmail.com>
2024-07-16 23:09:03 +0900

    feat : 불변성

---
+++ pragmatism/src/main/java/com/nareun130/pragmatism/object/vo/AccountInfo.java
@@ -0,0 +1,35 @@
+package com.nareun130.pragmatism.object.vo;
+
+import lombok.AllArgsConstructor;
+
+//* 불변성을 보여주는 코드
+@AllArgsConstructor
+public class AccountInfo {
+    private long mileage;
+
+    public AccountLevel getLevel() {
+        if (mileage > 100_000)
+            return AccountLevel.DIAMOND;
+        else if (mileage > 50_000)
+            return AccountLevel.GOLD;
+        else if (mileage > 30_000)
+            return AccountLevel.SILVER;
+        else if (mileage > 10_000)
+            return AccountLevel.BRONZE;
+        else
+            return AccountLevel.NONE;
+    }
+    // * setter로 인해 멀티 스레드 환경에서 불변성이 깨짐
+    // public void setMileage(long mileage){
+    // this.mileage = mileage;
+    // }
+
+    // * setter가 사라진 대신 변경 요청이 들어올 대 새로운 객체를 반환하는 메서드 추가
+    public AccountInfo withMileage(long mileage) {
+        return new AccountInfo(mileage);
+    }
+}
+
+enum AccountLevel {
+    DIAMOND, GOLD, SILVER, BRONZE, NONE;
+}
